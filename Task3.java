279ff579e284bcca988dfcb2549e352a7eeb80f1
Denis Gandzha <gandja.denis@yandex.ru>
2019-03-05 08:41:32 +0300

    changed source Task3

--- Task3.java
+++ Task3.java
@@ -1,8 +1,18 @@
 public class Task3 {
     public static void main(String[] args) {
         int[] arr = new int[]{5, 6, 9, 100500, 34, 23, 90, 100500, -30, 50};
-        int intSize = -2147483648;
-        int max = intSize;
+        int max = arr[0];
+        int count = 0;
+        int j = 0;
+        while(true){
+            if(arr[j] == 10050){
+                continue;
+            }
+            else {
+                max = arr[j];
+                break;
+            }
+        }
         for (int i = 0; i < arr.length; i++) {
 
             if(max < arr[i]) {
